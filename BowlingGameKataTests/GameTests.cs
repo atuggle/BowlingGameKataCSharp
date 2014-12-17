da75b78735fe5919efbb1d8bb0f88cf51c89b0b6
Allen Tuggle <atuggle@qualtrax.com>
2014-12-17 00:57:44 -0500

    Created Blank BowlingGameKata project and related Unit Test Project

---
+++ BowlingGameKataTests/GameTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameKataTests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
