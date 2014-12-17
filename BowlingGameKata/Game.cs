abfa588933b285472337361b4775c3153fb7ab69
Allen Tuggle <atuggle@qualtrax.com>
2014-12-17 01:19:45 -0500

    Implemented test CanBowlSpare

--- BowlingGameKata/Game.cs
+++ BowlingGameKata/Game.cs
@@ -15,7 +15,35 @@ namespace BowlingGameKata
 
         public Int32 Score()
         {
-            return rolls.ToList().Sum();
+            var score = 0;
+            var rollIndex = 0;
+
+            for (var frame=0; frame < 10; frame++)
+            {
+                if (FrameIsSpare(rollIndex))
+                    score += 10 + ScoreSpareBonus(rollIndex);
+                else
+                    score += ScoreRegularFrame(rollIndex);
+
+                rollIndex += 2;
+            }
+
+            return score;
+        }
+
+        private Int32 ScoreRegularFrame(Int32 rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1];
+        }
+
+        private Int32 ScoreSpareBonus(Int32 rollIndex)
+        {
+            return rolls[rollIndex + 2];
+        }
+
+        private bool FrameIsSpare(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
         }
     }
 }
--- BowlingGameKataTests/GameTests.cs
+++ BowlingGameKataTests/GameTests.cs
@@ -29,6 +29,20 @@ namespace BowlingGameKataTests
             Assert.AreEqual(20, score);
         }
 
+        [TestMethod]
+        public void CanBowlSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(2);
+            game.Roll(2);
+            RollMany(16, 0);
+
+            var score = game.Score();
+
+            Assert.AreEqual(16, score);
+        }
+
         private void RollMany(Int32 rolls, Int32 pins)
         {
             for (var roll=0; roll<rolls; roll++)
